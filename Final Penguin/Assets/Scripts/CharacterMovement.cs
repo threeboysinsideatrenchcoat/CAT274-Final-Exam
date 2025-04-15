0e9901d3e2ee7ad13b5e6cf43434bc9a43fd2616
DanTheNerd35 <vazquezd8@montclair.edu>
2025-04-15 18:28:28 -0400

    04/15

--- Final Penguin/Assets/Scripts/CharacterMovement.cs
+++ Final Penguin/Assets/Scripts/CharacterMovement.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class test : MonoBehaviour
+public class CharacterMovement : HealthScript
+
 {
-    public bool thiscomputersucks = true;
+    public float speed = 0.2f;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,56 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.position += Vector3.down * speed;
+            Debug.Log("S Pressed");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position += Vector3.left * speed;
+            Debug.Log("A Pressed");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += Vector3.right * speed;
+            Debug.Log("D Pressed");
+        }
+    }
+
+   // private void OnCollisionEnter2D(Collision2D collison)
+   // {  
         
+       // if (collison.gameObject.tag == "Enemy")//&& health = 0
+       // {
+         //   Destroy(player);
+         //   SceneManager.LoadScene("Main");
+       // }
+   // }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Gift" && Input.GetKey(KeyCode.Space))
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Fish")// && Health < 3)
+        {
+            Destroy(collision.gameObject); 
+            health = health + 1; 
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    { 
+        if(other.gameObject.tag == "Snowball")
+        {
+            health = health - 1; 
+            
+        }
     }
 }
---
+++ Final Penguin/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+using UnityEngine.SceneManagement;
+public class HealthScript : MonoBehaviour
+{
+    public string characterName; 
+    public int health; 
+    public TextMeshProUGUI displayHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Health();
+        DisplayHealth();
+    }
+     public void DisplayHealth()
+    {
+        displayHealth.text = "Health: " + health; 
+    }
+
+    public void Health()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("Main");
+            
+        }
+    }
+}
